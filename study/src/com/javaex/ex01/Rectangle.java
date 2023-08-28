a4a9d4bf571d570e61f4ce4086383e034498b6cf
sonddokki <sonsharo@naver.com>
2023-08-28 12:57:22 +0900

    0828 자바 파일 수정

---
+++ study/src/com/javaex/ex01/Rectangle.java
@@ -0,0 +1,16 @@
+package com.javaex.ex01;
+
+public class Rectangle {
+	
+	// 필드
+	private int width;
+	private int height;
+	private String fillColor;
+	private String lineColor;
+	
+	// 생성자
+	
+	// 메소드-gs
+	
+	// 메소드-일반
+}
