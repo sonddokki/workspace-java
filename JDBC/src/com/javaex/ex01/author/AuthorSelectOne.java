cdc00004aa00585eec6833df2a1bdde8cb79d5e5
sonddokki <sonsharo@naver.com>
2023-08-31 10:25:39 +0900

    0831 과제맞춰보기

--- JDBC/src/com/javaex/ex01/author/AuthorSelectOne.java
+++ JDBC/src/com/javaex/ex01/author/AuthorSelectOne.java
@@ -12,7 +12,7 @@ public class AuthorSelectOne {
 
 	public static void main(String[] args) {
 
-		List<AuthorVo> authorOneList = new ArrayList<AuthorVo>();
+		// List<AuthorVo> authorOneList = new ArrayList<AuthorVo>();
 
 		// 0. import java.sql.*;
 		Connection conn = null;
@@ -30,36 +30,46 @@ public class AuthorSelectOne {
 			// 3. SQL문 준비 / 바인딩 / 실행
 
 			// (1) SQL문 준비
+
 			String query = "";
 			query += " SELECT   author_id ";
 			query += "          ,author_name ";
 			query += "          ,author_desc ";
-			query += " FROM author ";			
+			query += " FROM author ";
+			query += " WHERE author_id = ? ";
 			System.out.println(query);
-			
-			pstmt = conn.prepareStatement(query);  // 변환
-			
-			// (2) 바인딩 -- > 물음표매칭 X 
-			
+
+			pstmt = conn.prepareStatement(query); // 변환
+
+			// (2) 바인딩 -- > 물음표매칭 X
+			int no = 3;
+
+			pstmt.setInt(1, no);
+
 			// (3) 실행
 			rs = pstmt.executeQuery();
-			
 
 			// 4.결과처리
-			while(rs.next()) {
-				int authorId = rs.getInt(1);
-				String authorName = rs.getString(2);
-				String authorDesc = rs.getString(3);
-				
-				AuthorVo authorVo = new AuthorVo(); // 작가 객체 생성
-				authorVo.setAuthorId(authorId);
-				authorVo.setAuthorName(authorName);
-				authorVo.setAuthorDesc(authorDesc);
-				
-				authorOneList.add(authorVo);
-			}
+			rs.next();
+			int authorId = rs.getInt(1);
+			String authorName = rs.getString(2);
+			String authorDesc = rs.getString(3);
+			
+			// authorVo 만들어서 담기
+			AuthorVo authorVo = new AuthorVo(authorId, authorName, authorDesc);
+
+//			System.out.print(" id: " + authorId);
+//			System.out.print(" name: " + authorName);
+//			System.out.println(" desc: " + authorDesc);
+			
+			// DB에서 데이터를 전부 가져와서 구분하면 자바에서 로직을 짜줘야함
+			// DB에서 걸러주면 하나 값 출력때는 리스트에 담지 않아도됨
+			// System.out.println(" desc: " + authorOneList.get(2).getAuthorDesc());
 			
 			
+			System.out.println(authorVo);
+//			System.out.println(authorVo.toString());
+			// toString이 있으면 자동으로 출력해줌
 
 		} catch (ClassNotFoundException e) {
 			System.out.println("error: 드라이버 로딩 실패 - " + e);
@@ -81,12 +91,7 @@ public class AuthorSelectOne {
 			} catch (SQLException e) {
 				System.out.println("error:" + e);
 			}
-			
-		
-			System.out.print( " id: " + authorOneList.get(2).getAuthorId());
-			System.out.print( " name: " + authorOneList.get(2).getAuthorName());
-			System.out.println( " desc: " + authorOneList.get(2).getAuthorDesc());
-			
+
 			
 
 		}
